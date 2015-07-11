f94e7b2a4e3f6ca6da9e1a7ef96895c8e4ad4994
sharvari03 <sharvari.nadkarni03@gmail.com>
2015-07-12 01:37:25 +0530

    01:36am

---
+++ CodeForGood/footer.php
@@ -0,0 +1,41 @@
+<div class="footer" id="footer" >
+	<div class="footerContent">
+	<!--<div class="circletag2" id="phone" style="margin-left:78px;">
+		<img src="assets/footer/phone.png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:11px;margin-top:6px"></img>
+	</div>
+	<div class="text">
+		<b>Contact us</b><br/>022-26555555
+	</div>-->
+	<a href="contact.php">
+		<div class="circletag2" id="phone" style="margin-left: 1%;">
+			<img src="assets/footer/message.png".png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:6px;margin-top:13px"></img>
+		</div>
+	</a>
+    <a href="contact.php">
+	<div class="text">
+		<b>Contact</b> <br/>enactushrcollege199@gmail.com
+	</div>
+    </a>
+	<div class="circletag2" id="phone" style="margin-left: 15%;">
+		<img src="assets/footer/plus.png".png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:8px;margin-top:7px"></img>
+	</div>
+	<div class="text">
+		<b>Follow us</b><br/>
+		<div class="icon" style="width:auto;height:auto;float:left;"><a href="https://www.facebook.com/enactushrcollege" target="_blank"><img src="assets/footer/fb.jpg".jpg" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:0px;margin-right:5px;margin-top:0px"></img></a></div>
+		<div class="icon" style="width:auto;height:auto;float:left;margin-top:8px;"><a href="https://instagram.com/enactusHRC" target="_blank" ><img src="assets/footer/ig.png".png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:0px;margin-right:5px;margin-top:0px"></img></a></div>
+		<div class="icon" style="width:auto;height:auto;float:left;margin-top:7px;"><a href="https://twitter.com/enactusHRC" target="_blank" ><img src="assets/footer/twitter.png".png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:0px;margin-top:0px"></img></a></div>
+	</div>
+	<div class="circletag2" id="phone" style="margin-left: 20%;">
+		<img src="assets/footer/copyr.png".png" style="display: table-cell;text-align: center;vertical-align: middle;margin-left:7px;margin-top:6px"></img>
+	</div>
+	<div class="text">
+		<b>Copyrights</b><br/>HR College
+	</div>
+	</div>
+	<div class="dev" style="float:right;margin-right:20px;text-decoration:none;font-family: MyFont;">
+		<a href="devpage.php" style="text-decoration: none; color: black;">Developers</a>
+	</div>
+    <div class="dev" style="float:left;font-family: MyFont;margin-left:20px;">
+		<a href="http://www.hrcollege.edu" target="_blank" style="text-decoration: none; color: black;">HR College Of Commerce & Economics</a>
+	</div>
+</div>
---
+++ CodeForGood/projects.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+		include('db_connect.php');
+		if(!$pid=$_GET['pid']);
+			$pid=1;
+		$count=0;
+		
+		$sql="Select count(*) from project";
+		$max = $con -> query($sql) or die("Error:".$sql);
+		while($row2 = $max->fetch_array())
+		{							
+			$abc=$row2[0];
+		}
+			//echo $pid;
+?>
+<html>
+    <head>
+        <title>Projects</title>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=1150">
+        
+        <link rel="stylesheet" type="text/css" href="css/projects/projectscss.css"/>
+        <!--Start, Santosh style -->
+        <link rel="stylesheet" type="text/css" href="css/header_footer/style1.css" />
+        <!--End, Santosh style -->
+        
+        <!--start, slider
+        	<link rel="stylesheet" href="css/projects/jquery.sliderTabs.css">
+        -->
+		<script src="js/jquery-2.1.1.min.js"></script>
+        <!--
+        	<script src="js/jquery.sliderTabs.js"></script>
+    	end, slider -->
+        
+        <!--  Load the stylesheets for image slider  -->
+    	<link rel="stylesheet" href="css/projects/slider/default.css" type="text/css" media="screen" />
+    	<link rel="stylesheet" href="css/projects/slider/nivo-slider.css" type="text/css" media="screen" />
+	    <link rel="stylesheet" href="css/projects/slider/style.css" type="text/css" media="screen" />
+        
+        <style type="text/css">
+			.demoPanel{
+			padding: 0;
+			background-color: #222;
+			text-align: center;
+			}
+			.demoPanel img{
+			width: 100%;
+			}
+			.ui-slider-tabs-indicator-container{
+			background-color: rgba(34, 34, 34, 0.32);
+			}
+		</style>
+        
+        <script>
+		$(document).ready(function() {
+         
+		
+		
+		 
+            $.ajax({
+
+                type: "GET",
+                url: "php/project/proj1.php",
+                data: 'q=1',
+                success: function(msg){
+				//console.log(msg);
+                    $('#from_ajax_with_json').append(msg);
+					
+					//console.log(i);
+                }
+
+            }); // Ajax Call
+      
+		
+		
+    }); //document.ready
+	var i=1;
+var count=6;
+var main=1
+var n=0;
+   
+	 $(document).ready(function() {
+        $('#right-arrow').click(function(){
+		
+		 i++;
+		 var a=(i%3);
+		 var b=(i+1)%3;
+		 var c=(i+2)%3;
+		// var d=(i+3)%4;
+		 if(a==0)
+		 {
+			a=3;
+			 
+		 }
+		 else if(b==0)
+		 {
+			 b=3;
+		 }
+		 else if(c==0)
+		 {
+			 c=3;
+		 }
+            $.ajax({
+
+                type: "GET",
+                url: "php/project/proj.php",
+                data: 'q=' + i,
+				dataType : 'json',
+                success: function(msg){
+                    $('#a_1').html(msg['ajax2']);
+					$('#a_2').html(msg['ajax4']);
+					$('#a_3').html(msg['ajax6']);
+					$('#p_1').attr('href','projects.php?pid='+msg['ajax1']+'');
+					$('#p_2').attr('href','projects.php?pid='+msg['ajax3']+'');
+					$('#p_3').attr('href','projects.php?pid='+msg['ajax5']+'');
+					$('#b_1').attr('class','circletag2_slider'+a+'');
+					$('#b_2').attr('class','circletag2_slider'+b+'');
+					$('#b_3').attr('class','circletag2_slider'+c+'');
+                }
+
+            }); // Ajax Call
+      
+		}); //event handler
+    }); //document.ready
+	$(document).ready(function() {
+        $('#left-arrow').click(function(){
+	
+		i--;
+		var a=(i%3);
+		 var b=(i+1)%3;
+		 var c=(i+2)%3;
+		// var d=(i+3)%4;
+		 if(a==0)
+		 {
+			a=3;
+			 
+		 }
+		 else if(b==0)
+		 {
+			 b=3;
+		 }
+		 else if(c==0)
+		 {
+			 c=3;
+		 }
+		if(i==0)
+		{
+			i=<?php echo $abc;?>
+		}		
+		
+            $.ajax({
+
+                type: "GET",
+                url: "php/project/proj.php",
+                data: 'q=' + i,
+				dataType : 'json',
+                 success: function(msg){
+                    $('#a_1').html(msg['ajax2']);
+					$('#a_2').html(msg['ajax4']);
+					$('#a_3').html(msg['ajax6']);
+					
+					$('#p_1').attr('href','projects.php?pid='+msg['ajax1']+'');
+					$('#p_2').attr('href','projects.php?pid='+msg['ajax3']+'');
+					$('#p_3').attr('href','projects.php?pid='+msg['ajax5']+'');
+					$('#b_1').attr('class','circletag2_slider'+a+'');
+					$('#b_2').attr('class','circletag2_slider'+b+'');
+					$('#b_3').attr('class','circletag2_slider'+c+'');
+					//console.log(i);
+					
+					if(i==1||i==0)
+					{
+						i=msg['ajax'];
+						i++;
+					}
+					
+                }
+
+            }); // Ajax Call
+       
+		}); //event handler
+    }); //document.ready
+	
+		</script>
+        
+    </head>
+    <body>
+		<?php
+        	include("header.php");
+		?> 
+        <div class="background">
+
+            <div class="structure">
+                
+
+
+                <div class="content">
+                    <div class="projectHead">Latest Projects</div>
+                    <div class="slider" style="width:981px;height:161px;border:0px solid;">
+					<div class="left-arrow" id="left-arrow" style="float:left;margin-left:225px;margin-top:62px;"><img src="assets/home_projects/arrow-left2.png"></div>
+						<div class="names" style="width:351px;height:160px;margin-left:25px;margin-right:25px;border:0px solid;float:left;">
+							<div class="from_ajax_with_json" id="from_ajax_with_json" >
+							
+							</div>
+						</div>
+					<div class="right-arrow" id="right-arrow"  style="float:left;margin-top:62px;">
+						<img src="assets/home_projects/arrow-right2.png">
+					</div>
+					</div>
+                    <div class="projectDesc">
+					<div id="wrapper" class="profilePicture">
+                        <!-- The slider wrapper div  -->
+                        <?php
+							if($pid==3)
+							{
+						?>
+								<div class="slider-wrapper theme-default" style="max-height: 315px;">
+						<?php
+							}
+							else
+							{
+						?>
+								<div class="slider-wrapper theme-default">
+						<?php
+							}
+						?>
+						
+                            <div id="slider" class="nivoSlider" style="min-height: 300px; max-height: 400px;">
+                        <?php
+                            //$pid=$_GET['pid'];
+                            $title1="Select * from project_images where p_id=".$pid ;
+                            $res1 = $con -> query($title1) or die("Error:".$title1);
+                            while($row1 = $res1->fetch_assoc())
+                            {
+                                $imageUrl=$row1['image'];
+                                $imageUrl = str_replace(":","-",$imageUrl);
+                                //echo '<li><a href="#'.$imageUrl.'"></a></li>';
+                            
+                                //  Images to slide through.
+                                echo '<img src="CMS/uploads/'.$imageUrl.'" alt="" style="-webkit-box-shadow: 0px 1px 5px 0px #4a4a4a; -moz-box-shadow: 0px 1px 5px 0px #4a4a4a; box-shadow: 0px 1px 5px 0px #4a4a4a;" />';
+                            }
+                        ?>
+                                <!--
+                                    <a href="http://dev7studios.com">
+                                    <img src="assets/project/slider/up.jpg" data-thumb="images/up.jpg" alt="" title="This is an example of a caption" />
+                                    <!--</a>
+                                    <img src="assets/project/slider/walle.jpg" data-thumb="images/walle.jpg" alt="" data-transition="slideInLeft" />
+                                    <img src="assets/project/slider/nemo.jpg" data-thumb="images/nemo.jpg" alt="" title="#htmlcaption" />
+                                -->
+                            </div>
+                            
+                            <!--  Captions to show for images  
+                                <div id="htmlcaption" class="nivo-html-caption">
+                                    <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>. 
+                                </div>
+                            -->
+                            
+                        </div>
+                
+                	</div>
+					<?php $title="Select * from project where p_id=".$pid ;
+						$res = $con -> query($title) or die("Error:".$title);
+      					while($row = $res->fetch_assoc())
+						{
+							
+							
+							
+                        /*<div class="profilePicture" id="imageSlider">
+							  <img alt="" style="margin-left: 0px; margin-top: 0px; width: 400px; height: 400px;" 
+							   src="assets/home_projects/enactusLogo.jpg" 
+							   id="profilePictureImage" class="profilePictureImage"> 
+							</div>*/
+							
+							/*
+								<div class="circleLinks">
+									<div class="circle"><p class="circletext">'.$pid.'</p></div>
+								
+								</div>
+							*/
+							
+							echo '<div class="contentRight">
+								
+							
+							
+								<div class="hello" id="projectTitle">'.$row["name"].'</div>
+								</div>
+								<p class="helloContent" style="line-height: 1.5em;">'.$row["description"].'</p>';
+						}
+					?>
+					<?php 
+						/*$title1="Select * from project_images where p_id=".$pid ;
+						$res1 = $con -> query($title1) or die("Error:".$title1);
+      					/*var_dump($res1);
+						echo "<br/>";*/
+						/*$title="Select * from project where p_id=".$pid ;
+						$res = $con -> query($title) or die("Error:".$title);
+      					while($row = $res->fetch_assoc())
+						{
+							echo '<div class="profilePicture" id="demoSlider"><ul>';
+							while($row1 = $res1->fetch_assoc())
+							{
+								$imageUrl=$row1['image'];
+                                $imageUrl = str_replace(":","-",$imageUrl);
+								echo '<li><a href="#'.$imageUrl.'"></a></li>';
+							}
+							echo '</ul>';
+							$title1="Select * from project_images where p_id=".$pid ;
+							$res1 = $con -> query($title1) or die("Error:".$title1);
+							while($row1 = $res1->fetch_assoc())
+							{	
+								$imageUrl=$row1['image'];
+                                $imageUrl = str_replace(":","-",$imageUrl);
+								echo '<div id="'.$imageUrl.'"><img src="CMS/uploads/'.$imageUrl.'" /></div>';
+							}
+							
+								echo '<div id="pyramid"><img src="assets/home_projects/enactusLogo.jpg" /></div>';
+							echo '</div>';*/
+							
+                        /*<div class="profilePicture" id="imageSlider">
+							  <img alt="" style="margin-left: 0px; margin-top: 0px; width: 400px; height: 400px;" 
+							   src="assets/home_projects/enactusLogo.jpg" 
+							   id="profilePictureImage" class="profilePictureImage"> 
+							</div>*/
+							
+							/*$title="Select * from project where p_id=".$pid ;
+							$res = $con -> query($title) or die("Error:".$title);
+							while($row = $res->fetch_assoc())
+							{
+								echo '<div class="profilePicture" id="demoSlider">
+								  <ul>
+									<li><a href="#pyramid"></a></li>
+									<li><a href="#gardens"></a></li>
+								  </ul>
+								
+								  <div id="pyramid"><img src="assets/home_projects/enactusLogo.jpg" /></div>
+								
+								  <div id="gardens"><img src="assets/home_projects/enactuslogo.png" /></div>
+								
+								</div>';
+							
+								echo '<div class="contentRight">
+								<div class="circleLinks">
+									<div class="circle"><p class="circletext">'.$pid.'</p></div>
+								
+								</div>
+								<div class="hello" id="projectTitle">'.$row["name"].'</div>
+								<div><pre class="helloContent">'.$row["description"].'</pre></div>';
+						}*/
+						?>
+                    <!--</div>                   -->
+                    <div style="clear:both">
+                    </div>
+                            
+                    </div>
+
+
+
+                </div>
+            </div>
+			
+            <!--<div class="footer">
+                <div class="footerContent"></div>
+            </div>-->
+        </div>
+        <?php
+        	include("footer.php");
+		?>
+        <!-- start, header active class -->
+        <!--<script src="js/jquery-2.1.1.min.js"></script>
+        <script>
+			$( document ).ready(function() {
+				 $("#ul_li_projects").addClass("active");
+			});
+		</script>-->
+        <!-- end, header active class -->  
+        <!--
+            <script>
+            var demo = $("div#demoSlider").sliderTabs({
+              autoplay: 4000,
+              height: 402,
+              indicators: true,
+              panelArrows: true,
+              panelArrowsShowOnHover: true,
+              transitionSpeed: 600,
+              tabs: false,
+              classes: {
+                panel: 'demoPanel'
+              }
+            });
+            </script>
+		-->
+                
+        <!--  Load the javascript files  -->
+		<script type="text/javascript" src="js/project_slider/jquery.nivo.slider.js"></script>
+        <script type="text/javascript">
+        
+			//<!--  Load the slider  --> 
+			$(window).load(function() {
+				$('#slider').nivoSlider();
+				//imageSlider.reload();
+			});
+        
+	    </script>  
+    </body>
+</html>
